32f2f9bd62786ba4f353a03a8af5ac48eda8b197
Ahmed Khalil <ahmed.khalil.bioinformatics@gmail.com>
2020-04-08 00:50:25 +0800

    Update bamBinning.m

--- CNAtraTool/@CNAtraLite/bamBinning.m
+++ CNAtraTool/@CNAtraLite/bamBinning.m
@@ -26,7 +26,7 @@ chrLengthsBAM = [fileInfo.SequenceDictionary.SequenceLength];
 chrNamesBAM   = [fileInfo.ScannedDictionary];
 
 %%%%--- Removing Chromosomes Y & M if exist ---%%%%
-chrListAccepted = {'chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX'};
+chrListAccepted = {'chr1','chr2','chr3','chr4','chr5','chr6','chr7','chr8','chr9','chr10','chr11','chr12','chr13','chr14','chr15','chr16','chr17','chr18','chr19','chr20','chr21','chr22','chrX','chrR'};
 %
 noChrsAccepted = 0;
 for i = 1:length(chrNamesBAM)
